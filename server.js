96a6a133f7fdf9211202bf2824f547ab53bdad7d
ray <coolrey3@gmail.com>
2019-07-05 15:54:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,61 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var mongoose = require ('mongoose');
+var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/swag-shop');
 
 var Product = require('./model/product');
 var Wishlist = require('./model/wishlist');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.get("/product", function(req,res){
-res.send("all is well");
-
+app.get("/product", function(req, res) {
+  Product.find({}, function(err, products) {
+    if (err) {
+      res.status(500).send({
+        error: "Could not fetch products"
+      });
+    } else {
+      res.send(products);
+    }
+  });
 });
 
-app.post("/product", function(req,res) {
-  var product = new Product();
-  product.title = req.body.title;
-  product.price = req.body.price;
-  product.save(function(err,savedProduct) {
 
+app.post("/wishlist", function(req, res) {
+  var wishList = new Wishlist();
+  wishList.title = req.body.title;
+  wishList.save(function(err, newWishList) {
     if (err) {
-      res.status(500).send({error:"could not save product"});
+      res.status(500).send({
+        error: "Could not create new wishlist"
+      });
     } else {
-      res.send(savedProduct);
+      res.send(newWishList);
     }
 
   });
-
-
-
-
 });
 
-app.listen(3000, function(){
+app.post("/product", function(req, res) {
+  var product = new Product();
+  product.title = req.body.title;
+  product.price = req.body.price;
+  product.save(function(err, savedProduct) {
+
+    if (err) {
+      res.status(500).send({
+        error: "could not save product"
+      });
+    } else {
+      res.send(savedProduct);
+    }
+  });
+});
+
+app.listen(3000, function() {
   console.log("Swag shop api running on port 3000");
 });
